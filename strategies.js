69e1e25d214bf2bc44d69f45c588eb0b87260df4
web3ten0 <web3ten0@gmail.com>
2022-01-18 10:49:17 +0900

    fix

--- strategies.js
+++ strategies.js
@@ -2,13 +2,13 @@ const Strategy = require('./strategies/bf/Strategy1')
 
 const strategies = [
   new Strategy({
-    minSpread: 1500,
-  }),
-  new Strategy({
-    minSpread: 2000,
+    minSpread: 500,
   }),
   new Strategy({
     minSpread: 750,
+  }),
+  new Strategy({
+    minSpread: 1250,
   }),
 ]
 
